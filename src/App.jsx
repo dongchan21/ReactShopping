2552db43d6af4de9ad6dcd5fa52d4083daa53faf
dongchan21 <dongchan5516@gmail.com>
2025-05-15 01:38:39 +0900

    250515

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import {Navbar, Container, Nav} from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import data from './data.js';
 
 function App() {
+
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
@@ -14,8 +20,28 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg"></div>
+
+    <Container>
+      <Row>
+        {shoes.map((a, i) => (
+          <Card shoes={shoes[i]} i={i+1}/>
+        ))}
+      </Row>
+    </Container>
+
     </div>
   );
 }
 
+function Card({shoe}) {
+  return (
+      <Col sm>
+      <img src={shoe.imageUrl} width="80%"/>
+      <h4>{shoe.title}</h4>
+      <p>{shoe.price}원</p>
+      </Col>
+  );
+}
 export default App
